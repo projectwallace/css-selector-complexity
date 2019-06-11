f759ea3432dd1664672c2430780997276a805177
Bart Veneman <bartveneman@gmail.com>
2019-06-12 00:02:55 +0200

    init

--- readme.md
+++ readme.md
@@ -1,30 +1,30 @@
 <div align="center">
-	<h1>PACKAGE_NAME</h1>
-	<p>PACKAGE_INTRODUCTION</p>
+	<h1>css-selector-complexity</h1>
+	<p>Calculate the Cyclomatic Complexity of a CSS selector</p>
 </div>
 
-[![NPM Version](https://img.shields.io/npm/v/PACKAGE_NAME.svg)](https://www.npmjs.com/package/PACKAGE_NAME)
-[![Weekly downloads](https://img.shields.io/npm/dw/PACKAGE_NAME.svg)](https://www.npmjs.com/package/PACKAGE_NAME)
-[![Build Status](https://travis-ci.org/bartveneman/PACKAGE_NAME.svg?branch=master)](https://travis-ci.org/bartveneman/PACKAGE_NAME)
-[![Known Vulnerabilities](https://snyk.io/test/github/bartveneman/PACKAGE_NAME/badge.svg)](https://snyk.io/test/github/bartveneman/PACKAGE_NAME)
-![Dependencies Status](https://img.shields.io/david/bartveneman/PACKAGE_NAME.svg)
-![Dependencies Status](https://img.shields.io/david/dev/bartveneman/PACKAGE_NAME.svg)
+[![NPM Version](https://img.shields.io/npm/v/PACKAGE_NAME.svg)](https://www.npmjs.com/package/css-selector-complexity)
+[![Weekly downloads](https://img.shields.io/npm/dw/css-selector-complexity.svg)](https://www.npmjs.com/package/css-selector-complexity)
+[![Build Status](https://travis-ci.org/bartveneman/css-selector-complexity.svg?branch=master)](https://travis-ci.org/bartveneman/css-selector-complexity)
+[![Known Vulnerabilities](https://snyk.io/test/github/bartveneman/css-selector-complexity/badge.svg)](https://snyk.io/test/github/bartveneman/css-selector-complexity)
+![Dependencies Status](https://img.shields.io/david/bartveneman/css-selector-complexity.svg)
+![Dependencies Status](https://img.shields.io/david/dev/bartveneman/css-selector-complexity.svg)
 [![XO code style](https://img.shields.io/badge/code_style-XO-5ed9c7.svg)](https://github.com/sindresorhus/xo)
 [![Project: Wallace](https://img.shields.io/badge/Project-Wallace-29c87d.svg)](https://www.projectwallace.com/oss)
 
 ## Installation
 
 ```sh
-npm install PACKAGE_NAME
+npm install css-selector-complexity
 ```
 
 ## Usage
 
 ```js
-// EXAMPLE CODE
+const complexity = require('css-selector-complexity')
+console.log(complexity('.my > #brutal ~ [css]:first-child + selector'))
+//-> 5
 ```
-
-## API
 
 ## Related projects
 
--- test/index.js
+++ test/index.js
@@ -1,3 +1,116 @@
 const test = require('ava')
+const loc = require('..')
 
-test.todo('WRITE TEST')
+test('it only expects strings', t => {
+	const provider = [1, null, undefined, -1, {}, [], false, '']
+
+	provider.map(notSelector => {
+		t.throws(() => loc(notSelector))
+	})
+})
+
+test('it counts simple selectors', t => {
+	const provider = [[1, '.class'], [1, 'element'], [1, '#id'], [0, '*']]
+
+	provider.map(([expected, selector]) => {
+		const actual = loc(selector)
+		t.is(
+			loc(selector),
+			expected,
+			`Expected ${selector} to have LOC of ${expected}, found ${actual}`
+		)
+	})
+})
+
+test('it counts combined selectors', t => {
+	const provider = [
+		[3, '.class .within .class'],
+		[2, 'ul > li'],
+		[2, 'dt ~ dd'],
+		[2, 'h1 + p'],
+		[2, 'test#me'],
+		[2, '#id.class'],
+		[4, '.class#id[attribute=value]']
+	]
+
+	provider.map(([expected, selector]) => {
+		const actual = loc(selector)
+		t.is(
+			actual,
+			expected,
+			`Expected ${selector} to have LOC of ${expected}, found ${actual}`
+		)
+	})
+})
+
+test('it counts pseudo selectors', t => {
+	const provider = [
+		[2, 'p:first-child'],
+		[2, 'a :only-child'],
+		[2, 'li:nth-child(2n+1)'], // @TODO: support spaces around operators: https://github.com/gajus/scalpel/issues/11
+		[2, 'p::first-letter']
+	]
+
+	provider.map(([expected, selector]) => {
+		const actual = loc(selector)
+		t.is(
+			actual,
+			expected,
+			`Expected ${selector} to have LOC of ${expected}, found ${actual}`
+		)
+	})
+})
+
+test('it counts attribute selectors', t => {
+	const provider = [
+		[1, '[aria-hidden]'],
+		[2, '[property|="value"]'],
+		[2, '[property^="value"]'],
+		[2, '[property$="value"]'],
+		[2, '[property*="value"]'],
+		[2, '[property="value"]']
+	]
+
+	provider.map(([expected, selector]) => {
+		const actual = loc(selector)
+		t.is(
+			actual,
+			expected,
+			`Expected ${selector} to have LOC of ${expected}, found ${actual}`
+		)
+	})
+})
+
+test('it counts insane real-world cases', t => {
+	const provider = [
+		// Carbon Design System
+		[
+			13, // Should be 14, but counting inside :not() is hard
+			'.bx--side-nav--website--light .bx--side-nav__menu[role=menu] a.bx--side-nav__link[role=menuitem]:not(.bx--side-nav__link--current):not([aria-current=page]):hover .bx--side-nav__icon svg'
+		],
+		// Smashing Magazine
+		[
+			8,
+			'.dashboard-membership__desc.for-supporters li.for-members.for-smashing:not(.for-supporters) time::after'
+		],
+		// CSS Tricks
+		[
+			10,
+			'body:not(.woocommerce-page):not(.page-template-page-search-results) .overflow-table-wrap>table.overflow-table:not(.gsc-table-result):not(.gcsc-branding):not(.gsc-resultsHeader) table'
+		],
+		// Google Cloud
+		[
+			14,
+			'body.dropdown-available[data-navigation="dropdown"] .devsite-doc-set-nav-tab-container:nth-child(6) .devsite-dropdown-menu-column:nth-child(1) .devsite-nav-item:nth-child(3) a.devsite-nav-title span::after'
+		]
+	]
+
+	provider.map(([expected, selector]) => {
+		const actual = loc(selector)
+		t.is(
+			actual,
+			expected,
+			`Expected ${selector} to have LOC of ${expected}, found ${actual}`
+		)
+	})
+})
